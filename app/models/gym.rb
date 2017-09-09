b58401786aac344eb425f4e5fafd4a1760aaa898
Italo Pires <italonapalm@gmail.com>
2017-09-09 14:00:25 -0300

    changed name constant QTT_PER_PAGE to PER_PAGE_ITEMS on gym model

--- app/models/gym.rb
+++ app/models/gym.rb
@@ -1,12 +1,12 @@
 class Gym < ApplicationRecord
 
   # Constants
-  QTT_PER_PAGE = 6
+  PER_PAGE_ITEMS = 6
 
   validates_presence_of :name
 
   # Scopes
   scope :descending_order, -> (page) {
-    order(created_at: :desc).page(page).per(QTT_PER_PAGE)
+    order(created_at: :desc).page(page).per(PER_PAGE_ITEMS)
   }
 end
